92e3116f5d547a079d6313ae7413ea8c5ef179df
mohamed adel <adel@buseet.com>
2020-04-05 17:52:59 +0200

    update some pakages

---
+++ tests/Feature/InitialTest.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dola
+ * Date: 04-Jan-19
+ * Time: 11:32 PM
+ */
+
+namespace adel\laravel_package\Tests;
+
+
+use Orchestra\Testbench\TestCase;
+
+class InitialTest extends TestCase
+{
+
+    public function my_first_test()
+    {
+        $this->assertTrue(true);
+    }
+
+}
